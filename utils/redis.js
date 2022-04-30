a9c76fc4e9afb8c611c9e6b4967324441dcae47b
faykris <faykris28@gmail.com>
2022-04-30 18:31:29 -0500

    [Update] redis.js: fix eslint

--- utils/redis.js
+++ utils/redis.js
@@ -1,21 +1,20 @@
 import redis from 'redis';
 import { promisify } from 'util';
 
-const redisClient = class RedisClient {
-  client = redis.createClient();
-  GET_ASYNC = promisify(this.client.get).bind(this.client);
-  SET_ASYNC = promisify(this.client.set).bind(this.client);
-  DEL_ASYNC = promisify(this.client.del).bind(this.client);
-  EXP_ASYNC = promisify(this.client.expire).bind(this.client);
-
+const RedisClient = class RedisClient {
   constructor() {
+    this.client = redis.createClient();
+    this.GET_ASYNC = promisify(this.client.get).bind(this.client);
+    this.SET_ASYNC = promisify(this.client.set).bind(this.client);
+    this.DEL_ASYNC = promisify(this.client.del).bind(this.client);
+    this.EXP_ASYNC = promisify(this.client.expire).bind(this.client);
     this.client.on('error', (err) => {
-        this.connection = false;
-        console.log(err);
-      })
+      this.connection = false;
+      console.log(err);
+    })
       .on('connect', () => {
         this.connection = true;
-    });
+      });
   }
 
   isAlive() {
@@ -23,7 +22,7 @@ const redisClient = class RedisClient {
   }
 
   async get(key) {
-    return await this.GET_ASYNC(key);
+    return this.GET_ASYNC(key);
   }
 
   async set(key, value, duration) {
@@ -35,6 +34,6 @@ const redisClient = class RedisClient {
   async del(key) {
     await this.DEL_ASYNC(key);
   }
-}
+};
 
-export default new redisClient();
+export default new RedisClient();
