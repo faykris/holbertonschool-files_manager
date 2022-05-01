5c2e80f76cb458108442a755fa97d77fcb14fd65
faykris <faykris28@gmail.com>
2022-05-01 08:32:25 -0500

    [Update] UsersController.js, index.js, server.js: fix eslint

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,43 +1,46 @@
-import dbClient from '../utils/db';
 import crypto from 'crypto';
-import redisClient from "../utils/redis";
-
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 const ObjectId = require('mongodb').ObjectID;
 
-const usersController = class UsersController {
+const UsersController = class UsersController {
+  constructor() {
+    this.collection = 'users';
+  }
+
   async postNew(email, password) {
-    if (!email) return {error: 'Missing email'};
-    if (!password) return {error: 'Missing password'};
+    if (!email) return { error: 'Missing email' };
+    if (!password) return { error: 'Missing password' };
 
     const db = await dbClient.client.db(dbClient.database);
-    const collection = await db.collection('users');
-    const nUsers = await collection.find({email: email}).toArray();
+    const collection = await db.collection(this.collection);
+    const nUsers = await collection.find({ email }).toArray();
 
-    if (nUsers.length > 0) return {error: 'Already exist'};
+    if (nUsers.length > 0) return { error: 'Already exist' };
 
     const sha = crypto.createHash('sha1');
     sha.update(password);
     const hashPass = sha.digest('hex');
-    const result = await collection.insertOne({email: email, password: hashPass});
+    const result = await collection.insertOne({ email, password: hashPass });
 
-    return { id: result.insertedId, email: email };
+    return { id: result.insertedId, email };
   }
 
   async getMe(token) {
-    const authToken = `auth_${token}`
+    const authToken = `auth_${token}`;
     const userId = await redisClient.get(authToken);
 
     if (userId !== null) {
       const db = await dbClient.client.db(dbClient.database);
-      const collection = await db.collection('users');
-      const user = await collection.findOne({_id: new ObjectId(userId)});
+      const collection = await db.collection(this.collection);
+      const user = await collection.findOne({ _id: new ObjectId(userId) });
       if (user !== null) {
-        return { id : user._id, email : user.email }
+        return { id: user._id, email: user.email };
       }
     }
-    return { error : 'Unauthorized' };
+    return { error: 'Unauthorized' };
   }
-}
+};
 
-export default new usersController();
+export default new UsersController();
